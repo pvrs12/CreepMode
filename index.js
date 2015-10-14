ed8f383e75436edd4b9b32c73f6b03fe8dd77435
Patrick Vares <pvrs12@gmail.com>
2015-10-14 00:51:32 -0400

    added icons and icon code, doesn't work

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 var self = require('sdk/self');
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var { ActionButton } = require('sdk/ui/button/action');
+
+const enabledState = {
+	"label": "Turn CreepMode Off",
+	"icon":{
+		"16":"./icons/enabled-16.png",
+		"32":"./icons/enabled-32.png",
+		"64":"./icons/enabled-64.png",
+	},
 }
 
-exports.dummy = dummy;
+const disabledState={
+	"label": "Turn CreepMode On",
+	"icon":{
+		"16":"./icons/disabled-16.png",
+		"32":"./icons/disabled-32.png",
+		"64":"./icons/disabled-64.png",
+	},
+}
+
+var button = ActionButton({
+	id: "toggle-button",
+	label: "Turn CreepMode On",
+	icon:{
+		"16":"./icons/disabled-16.png",
+		"32":"./icons/disabled-32.png",
+		"64":"./icons/disabled-64.png",
+	},
+	onClick:function(state){
+		console.log("pushed!");
+		if(button.label="Turn CreepMode On"){
+			button.state(button,enabledState);
+		} else {
+			button.date(button,disabledSatate);
+		}
+	}
+});
