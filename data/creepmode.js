bca473b6c42dfa2645e3c52ce92b93278b76fe15
Patrick Vares <pvrs12@gmail.com>
2015-10-15 01:02:03 -0400

    preliminary working system

--- data/creepmode.js
+++ data/creepmode.js
@@ -1,5 +1,19 @@
 var oldInnerHTML = window.document.body.innerHTML;
 
+var modifying = false;
+var enabled = false;
+
+var hideElements=[
+	'_5gl-',//photobar
+	'_32jt',//addfriend
+	'share_root',//share
+	'comment_link',//commentlink
+	'UFIAddComment',//commentbox
+	'UFILikeLink',//likelink
+	'FriendRequestAdd',//friendrequest button
+	'addButton',//friendrequest button
+]
+
 self.port.on('disabled',function(){
 	creepModeOff();
 });
@@ -8,35 +22,38 @@ self.port.on('enabled',function(){
 	creepModeOn();
 });
 
+document.body.addEventListener("DOMSubtreeModified",function(){
+	if(!modifying){
+//		console.log('tree modified! '+new Date());
+		hideStuff();
+	}
+},false);
+
 function creepModeOff(){
+	enabled = false;
+	modifying=true;
 	window.document.body.innerHTML=oldInnerHTML;
+	modifying=false;
 	console.log("Creep Mode turned off");
 }
 
 function creepModeOn(){
+	enabled = true;
 	oldInnerHTML = window.document.body.innerHTML;
 	console.log("Creep Mode turned on");
 
-	hideLikes();
-	hideCommentBox();
-	hideCommentLink();
-	hideShare();
+	hideStuff();
 }
 
-function hideLikes(){
-	hideClassElements('UFILikeLink');
-}
-
-function hideCommentBox(){
-	hideClassElements('UFIAddComment');
-}
-
-function hideCommentLink(){
-	hideClassElements('comment_link');
-}
-
-function hideShare(){
-	hideClassElements('share_root');
+function hideStuff(){
+	if(enabled){
+		modifying=true;
+		for(var i=0;i<hideElements.length;++i){
+//			console.log('hiding \''+hideElements[i]+'\'');
+			hideClassElements(hideElements[i]);
+		}
+		modifying=false;
+	}
 }
 
 function hideClassElements(className){
@@ -45,5 +62,4 @@ function hideClassElements(className){
 	for(var i=0;i<elements.length;++i){
 		elements[i].style.display="none";
 	}
-
 }
