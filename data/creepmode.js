c88a866bebf35dffcf97a0547acf31c1ec1dbecf
Patrick Vares <pvrs12@gmail.com>
2015-10-15 14:25:01 -0400

    fixed pagelikebutton casing

--- data/creepmode.js
+++ data/creepmode.js
@@ -10,7 +10,7 @@ var hideElements=[
 	'UFILikeLink',//likelink
 	'FriendRequestAdd',//friendrequest button
 	'addButton',//friendrequest button
-	'pageLikeButton', //like button on pages
+	'PageLikeButton', //like button on pages
 ]
 
 self.port.on('disabled',function(){
